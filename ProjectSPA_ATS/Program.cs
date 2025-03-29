44903c738414c45eaafdf26a689d6198da14e916
Dawid Dluski <dawiddluski5@gmail.com>
2025-03-29 12:33:24 +0100

    fix

---
+++ ProjectSPA_ATS/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjectSPA_ATS.AST;
+using ProjectSPA_ATS.Lexer;
+using ProjectSPA_ATS.Parser;
+
+namespace SimpleSPA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Przykładowy kod w języku SIMPLE
+            string sourceCode = @"
+procedure Example {
+    x = 5;
+    while x {
+        x = x + 1;
+    }
+}
+";
+
+            // 1. Tworzymy obiekt Lexer i wyciągamy tokeny
+            var lexer = new Lexer(sourceCode);
+            List<Token> tokens = lexer.GetTokens(sourceCode);
+
+            Console.WriteLine("== TOKENS ==");
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+
+            // 2. Parsujemy listę tokenów, otrzymujemy obiekt AST
+            var parser = new Parser(tokens);
+            ProcedureNode root = parser.ParseProcedure();
+
+            // 3. Wypisujemy AST
+            Console.WriteLine("\n== AST ==");
+            PrintAst(root);
+        }
+
+        /// <summary>
+        /// Drukuje AST rekurencyjnie
+        /// </summary>
+        static void PrintAst(AstNode node, int indent = 0)
+        {
+            string pad = new string(' ', indent * 2);
+
+            switch (node)
+            {
+                case ProcedureNode proc:
+                    Console.WriteLine($"{pad}Procedure '{proc.Name}':");
+                    foreach (var stmt in proc.Statements)
+                    {
+                        PrintAst(stmt, indent + 1);
+                    }
+                    break;
+
+                case AssignNode assign:
+                    Console.WriteLine($"{pad}Assign: {assign.VarName} = ");
+                    PrintAst(assign.Expression, indent + 1);
+                    break;
+
+                case WhileNode wh:
+                    Console.WriteLine($"{pad}While: {wh.ConditionVar}");
+                    Console.WriteLine($"{pad}{{");
+                    foreach (var stmt in wh.Body)
+                    {
+                        PrintAst(stmt, indent + 2);
+                    }
+                    Console.WriteLine($"{pad}}}");
+                    break;
+
+                case BinaryOpNode binOp:
+                    Console.WriteLine($"{pad}BinaryOp '{binOp.Operator}':");
+                    Console.WriteLine($"{pad}  Left:");
+                    PrintAst(binOp.Left, indent + 2);
+                    Console.WriteLine($"{pad}  Right:");
+                    PrintAst(binOp.Right, indent + 2);
+                    break;
+
+                case VarRefNode varRef:
+                    Console.WriteLine($"{pad}VarRef '{varRef.VarName}'");
+                    break;
+
+                case ConstNode constNode:
+                    Console.WriteLine($"{pad}Const {constNode.Value}");
+                    break;
+
+                default:
+                    Console.WriteLine($"{pad}[Nieznany węzeł AST typu: {node?.GetType().Name}]");
+                    break;
+            }
+        }
+    }
+}
