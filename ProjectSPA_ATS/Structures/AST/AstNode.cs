d942616d21e1c8d4c6067530b4ee4cfbbbc12a8c
DaV <DaV@DESKTOP-GH660AU>
2025-04-04 20:56:08 +0200

    Move to other folder

--- ProjectSPA_ATS/Structures/AST/AstNode.cs
+++ ProjectSPA_ATS/Structures/AST/AstNode.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectSPA_ATS.AST
+namespace ProjectSPA_ATS.Structures.AST
 {
-    public abstract class ExpressionNode : AstNode { }
+    public abstract class AstNode { }
 }
