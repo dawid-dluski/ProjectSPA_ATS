4814b82907f4daab3f665642779bdeb49b46e9ab
Skritek35 <kacpik005@gmail.com>
2025-05-27 20:50:54 +0200

    Add files via upload

---
+++ ProjectSPA_ATS/Structures/AST/CallNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSPA_ATS.Structures.AST
+{
+    public class CallNode: StatementNode
+    {
+        public string Callee { get; }
+
+        public CallNode(string callee) => Callee = callee;
+    }
+}
---
+++ ProjectSPA_ATS/Structures/AST/IfNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSPA_ATS.Structures.AST
+{
+    public class IfNode: StatementNode
+    {
+        public string ConditionVar { get; }
+        public List<StatementNode> ThenBody { get; }
+        public List<StatementNode> ElseBody { get; }
+
+        public IfNode(string cond, List<StatementNode> thenBody, List<StatementNode> elseBody)
+        {
+            ConditionVar = cond;
+            ThenBody = thenBody;
+            ElseBody = elseBody;
+        }
+    }
+}
